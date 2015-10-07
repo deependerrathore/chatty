beed100aa13d6dd1a23e1f9ff13f5d442d6e3c9a
depeenderrathore <deepenrathore28@gmail.com>
2015-10-07 23:23:32 +0530

    Showing the alert on home page

--- chatty/app/Http/routes.php
+++ chatty/app/Http/routes.php
@@ -15,3 +15,6 @@ Route::get('/',[
 	'uses' => '\Chatty\Http\Controllers\HomeController@index',
 	'as' => 'home'
 	]);
+Route::get('/alert', function(){
+	return redirect()->route('home')->with('info','You have signed up!');
+});
