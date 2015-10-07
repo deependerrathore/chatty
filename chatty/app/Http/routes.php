e2f7c0c1aee62c76281dfaba918ed46302fa9b02
depeenderrathore <deepenrathore28@gmail.com>
2015-10-08 00:51:48 +0530

    Sign up page is working fine

--- chatty/app/Http/routes.php
+++ chatty/app/Http/routes.php
@@ -13,8 +13,18 @@
 
 Route::get('/',[
 	'uses' => '\Chatty\Http\Controllers\HomeController@index',
-	'as' => 'home'
+	'as' => 'home',
 	]);
 Route::get('/alert', function(){
 	return redirect()->route('home')->with('info','You have signed up!');
 });
+
+/*Authentication*/
+Route::get('/signup',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@getSignup',
+	'as' => 'auth.signup',
+	]);
+
+Route::post('/signup',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@postSignup',
+	]);
