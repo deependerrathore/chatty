4d2f2d33ea605c0068e62527b774a4b877763614
depeenderrathore <deepenrathore28@gmail.com>
2015-10-09 00:52:39 +0530

    Sign out

--- chatty/app/Http/Controllers/AuthController.php
+++ chatty/app/Http/Controllers/AuthController.php
@@ -47,4 +47,10 @@ class AuthController extends Controller{
 
 		return redirect()->route('home')->with('info','You are now signed in.');
 	}
+
+	public function getSignout(){
+		Auth::logout();
+
+		return redirect()->route('home');
+	}
 }
