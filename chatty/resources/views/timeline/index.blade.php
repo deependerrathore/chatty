6fbaefc20d236cb9cdc3582d94530be8d0fde425
depeenderrathore <deepenrathore28@gmail.com>
2015-10-13 00:08:19 +0530

    Output like count

--- chatty/resources/views/timeline/index.blade.php
+++ chatty/resources/views/timeline/index.blade.php
@@ -36,8 +36,9 @@
 				                <li>{{ $status->created_at->diffForHumans() }}</li>
 				                @if ($status->user->id !== Auth::user()->id)
 					                <li><a href="{{ route('status.like',['statusId' => $status->id]) }}">Like</a></li>
-					                <li>10 likes</li>
-				                @endif 
+					                
+				                @endif
+				                <li>{{ $status->likes->count() }} {{ str_plural('like',$status->likes->count()) }}</li>
 				            </ul>
 				     		
 				     		@foreach ($status->replies as $reply)
@@ -52,8 +53,9 @@
 					                        <li>{{ $reply->created_at->diffForHumans() }}</li>
 					                        @if ($reply->user->id !== Auth::user()->id)
 						                        <li><a href="{{ route('status.like',['statusId' => $reply->id]) }}">Like</a></li>
-						                        <li>4 likes</li>
+						                        
 					                        @endif
+					                        <li>{{ $reply->likes->count() }} {{ str_plural('like',$reply->likes->count()) }}</li>
 					                    </ul>
 					                </div>
 					            </div>
